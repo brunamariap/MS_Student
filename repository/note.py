e8157d2525f811fb5184dce3415acaa4e05be558
Bruna Maria <bruna.mariap@outlook.com>
2023-11-06 13:38:50 -0300

    Organização dos models e alguns ajustes de formatação

--- repository/note.py
+++ repository/note.py
@@ -1,22 +1,23 @@
 from prisma.models import Note
 from prisma.partials import NoteRequest
 
-class NoteRepository:
-  
-  def __init__(self):
-      self.repository = Note
-      
-  def create(self, request: NoteRequest):
-    return self.repository.prisma().create(request)
-		
-  def get_all(self):
-    return self.repository.prisma().find_many()
-		
-  def get_by_id(self):
-    pass
 
-  def change(self, id: str, request: NoteRequest):
-    return self.repository.prisma().update(data=request, where={'id': id})
-  
-  def remove(self, id: str):
-    return self.repository.prisma().delete({ 'id': id })
+class NoteRepository:
+
+    def __init__(self):
+        self.repository = Note
+
+    def create(self, request: NoteRequest):
+        return self.repository.prisma().create(request)
+
+    def get_all(self):
+        return self.repository.prisma().find_many()
+
+    def get_by_id(self):
+        pass
+
+    def change(self, id: str, request: NoteRequest):
+        return self.repository.prisma().update(data=request, where={'id': id})
+
+    def remove(self, id: str):
+        return self.repository.prisma().delete({'id': id})
--- services/note.py
+++ services/note.py
@@ -1,22 +1,23 @@
 from repository.note import NoteRepository
 from prisma.partials import NoteRequest
 
+
 class NoteService:
-		
-		def __init__(self):
-				self.service = NoteRepository()
-				
-		def create(self, request: NoteRequest):
-				return self.service.create(request)
-		
-		def get_all(self):
-			return self.service.get_all()
-		
-		def get_by_id(self, id: str):
-			return self.service.get_by_id(id)
 
-		def change(self, id: str, request: NoteRequest):
-			return self.service.change(id, request)
+    def __init__(self):
+        self.service = NoteRepository()
 
-		def remove(self, id: str):
-			return self.service.remove(id)
+    def create(self, request: NoteRequest):
+        return self.service.create(request)
+
+    def get_all(self):
+        return self.service.get_all()
+
+    def get_by_id(self, id: str):
+        return self.service.get_by_id(id)
+
+    def change(self, id: str, request: NoteRequest):
+        return self.service.change(id, request)
+
+    def remove(self, id: str):
+        return self.service.remove(id)
