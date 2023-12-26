d5a0580d1bb260ca4df7bddcdde8eed8492e7b3c
Bruna Maria <bruna.mariap@outlook.com>
2023-12-26 13:37:42 -0300

    Mudança de nomes de variáveis

--- controllers/student.py
+++ controllers/student.py
@@ -10,6 +10,21 @@ import requests
 router = APIRouter(prefix="/students", tags=['Estudante'])
 student_service = StudentService()
 
+academic_management_MS_url_base = 'http://127.0.0.1:8001'
+
+def check_if_discipline_exists(disciplineId:str):
+    try:
+        url = f"{academic_management_MS_url_base}/disciplines/{disciplineId}/details"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return True
+        elif not response:
+            return JSONResponse(content={"details": "Não foi encontrado uma disciplina com o id especificado"}, status_code=status.HTTP_404_NOT_FOUND)
+
+    except Exception as error:
+        return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
+
 
 @router.get("/all")
 def list_students() -> List[StudentResponse]:
@@ -21,7 +36,7 @@ def list_students() -> List[StudentResponse]:
 @router.post("/create")
 def insert_student(request: StudentRequest) -> StudentResponse:
     try:
-        url = f"http://127.0.0.1:8001/classes/{request.dict()['classId']}/details"
+        url = f"{academic_management_MS_url_base}/classes/{request.dict()['classId']}/details"
         response = requests.get(url=url)
         if response.status_code == 200:
             response = student_service.create(request.dict())
@@ -48,7 +63,7 @@ def get_student(id: str) -> List[StudentResponse]:
 @router.put("/{id}/modify")
 def modify_student(id: str, request: StudentRequest) -> StudentResponse:
     try:
-        url = f"http://127.0.0.1:8001/classes/{request.dict()['classId']}/details"
+        url = f"{academic_management_MS_url_base}/classes/{request.dict()['classId']}/details"
         response = requests.get(url=url)
         if response.status_code == 200:
             response = student_service.change(id, request.dict())
@@ -72,10 +87,10 @@ def remove_student(id: str) -> StudentResponse:
         return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
 
 
-@router.get("/{studentId}/events/all")
-def get_student_event_participated(studentId: str) -> List[StudentParticipatesResponse]:
+@router.get("/{id}/events/all")
+def get_student_event_participated(id: str) -> List[StudentParticipatesResponse]:
     try:
-        response = student_service.get_event_participated(studentId)
+        response = student_service.get_event_participated(id)
 
         if not response:
             return JSONResponse(content={"details": "Não foi encontrado estudante com o id especificado"}, status_code=status.HTTP_404_NOT_FOUND)
@@ -88,7 +103,7 @@ def get_student_event_participated(studentId: str) -> List[StudentParticipatesRe
 
 @router.delete("/{id}/events/remove")
 def remove_student_link_to_event(id: str):
-    # try:
+    try:
         response = student_service.remove_student_link_to_event(id)
 
         if not response:
@@ -96,8 +111,8 @@ def remove_student_link_to_event(id: str):
 
         return JSONResponse(content={}, status_code=status.HTTP_204_NO_CONTENT)
 
-    # except Exception as error:
-    #     return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
+    except Exception as error:
+        return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
 
 
 @router.post("/events/link")
@@ -124,10 +139,10 @@ def get_all_student_and_events():
         return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
 
 
-@router.get("/{studentId}/disciplines/all")
-def get_student_disciplines(studentId: str):
+@router.get("/{id}/disciplines/all")
+def get_student_disciplines(id: str):
     try:
-        response = student_service.get_student_disciplines(studentId)
+        response = student_service.get_student_disciplines(id)
 
         if not response:
             return JSONResponse(content={"details": "Não foi encontrado um estudante com o id especificado"}, status_code=status.HTTP_404_NOT_FOUND)
@@ -137,15 +152,23 @@ def get_student_disciplines(studentId: str):
         return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
 
 
-@router.post("/disciplines/create")
-def create_link_student_discipline(studentId: str, request: List[StudentDisciplinesRequest]):
+@router.post("/{id}/disciplines/create")
+def create_link_student_discipline(id: str, request: List[StudentDisciplinesRequest]):
     try:
         formated_request = []
         for item in request:
-            formated_request.append(item.dict())
+            data = item.dict()
+            formated_request.append(data)
+            # print(data["disciplineId"])
+            # response = check_if_discipline_exists(data['disciplineId'])
+
+            # if response != True:
+            #     print('entrou')
+            #     return response
+
         response = student_service.create_link_student_discipline(
             formated_request)
-        
+
         return JSONResponse(content=jsonable_encoder(formated_request), status_code=status.HTTP_201_CREATED)
     except Exception as error:
         return JSONResponse(content=jsonable_encoder(error), status_code=status.HTTP_400_BAD_REQUEST)
